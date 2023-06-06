bb2ae2c7e94f51c28a8eefa856d3fcaddcaa712d
Joaquin <joro2509@gmail.com>
2023-06-07 00:28:32 +0200

    07

--- src/components/share-tools/BaseFront.vue
+++ src/components/share-tools/BaseFront.vue
@@ -1,15 +1,54 @@
 <template>
   <div class="front">
     <div class="front-part" id="left">
-      <div class="left-title-container">
-        <h1 class="left-title">{{ songs.data.title }}</h1>
+      <img :src="songs.image" alt="" class="disc" />
+      <div class="repro-container">
+        <div class="left-title">
+          <h2 class="title">{{ songs.data.title }}</h2>
+          <p class="release-title">Relesed: {{ songs.data.release_date }}</p>
+          <p class="label-title">Label: {{ songs.data.label }}</p>
+        </div>
+        <div class="repro">
+          <audio controls class="audio"><source src="" /></audio>
+          <div class="button-container">
+            <button
+              @click="songs.setSongs()"
+              type="button"
+              class="repro-button-back"
+            >
+              <img
+                src="@/assets/images/arrow-back.png"
+                class="arrow-img"
+                alt=""
+              />
+            </button>
+            <button
+              @click="songs.setSongs()"
+              type="button"
+              class="repro-button-next"
+            >
+              <img
+                src="@/assets/images/arrow-next.png"
+                class="arrow-img"
+                alt=""
+              />
+            </button>
+          </div>
+        </div>
       </div>
+    </div>
+    <div class="front-part" id="right">
       <ul class="listed">
-        <li class="unique-song" v-for="item in songs.songs" :key="item">
+        <li class="unique-song" v-for="item in songs.tracks" :key="item">
           <div class="description-song">
-            <div class="description-right">
+            <div class="description-left">
               <div class="button-play-container">
-                <button @click.prevent="playSound(item.preview)">Play</button>
+                <button
+                  class="play-button"
+                  @click.prevent="playSound(item.preview)"
+                >
+                  Play
+                </button>
               </div>
             </div>
             <div class="description-center">
@@ -17,77 +56,21 @@
                 <h2 class="song-title">{{ item.title }}</h2>
                 <h3 class="song-artist">{{ item.artist.name }}</h3>
               </div>
-              <div class="description-left">
-                <h3 class="song-detail">{{ item.duration }}</h3>
-                <img src="@/assets/like.png" alt="" class="like" />
+              <div class="description-right">
+                <h3 class="song-detail">
+                  {{ songs.data.duration }}
+                </h3>
+                <img src="@/assets/images/like.png" alt="" class="like" />
               </div>
             </div>
           </div>
         </li>
       </ul>
     </div>
-    <div class="front-part" id="center">
-      <img :src="songs.data.cover_medium" alt="" class="disc" />
-      <div class="center-title">
-        <h2 class="subtitle">{{ songs.data.title }}</h2>
-      </div>
-      <div class="repro">
-        <h2 class="title">---Barra tiempo---</h2>
-        <div class="button-container">
-          <button onclick="playVid()" type="button">Back</button>
-          <button onclick="playVid()" type="button">Play</button>
-          <button onclick="pauseVid()" type="button">Pause</button><br />
-          <button onclick="playVid()" type="button">Next</button>
-        </div>
-      </div>
-    </div>
-    <div class="front-part" id="right">
-      <div class="right-title-container">
-        <h1 class="right-title">Letra</h1>
-      </div>
-      <div class="lyrics-container">
-        <p class="lyrics">
-          No paran de llegar Billetes vienen y billetes van Pisando los Jordan
-          Yo ando listo, ustedes digan que plan Patrón Del Mal el mood Quieren
-          mi hielo y también el champú De Medallo a Moscú Se grabó en cuatro y
-          lo subió a YouTube Las morras me besan, pues las vuelvo locas a todas
-          Se besan, ellas bailan y se alborotan No ocupan flores, solo un polvo
-          que es rosa Y sigo bendecido, con lo mío en el camino, oh Puro Doble
-          P, a la verga, wey, jaja Desde Medallo, ¿sí sabe?, Jeje ¡Chau! (Hecho
-          en Medellín) Desde el barrio Antioquia Con El Bendito Pura Doble P
-          Baguettes en mi pulsera En mi cadena y mis anillos No los heredé de
-          niño, tuve que luchar Por eso chimba detrás mío Porque saben lo que
-          cargo Que traigo billete largo Hay mucho pa' gastar Morras encima de
-          mí, de mi ganga Y de to' lo que traigo en mi cuello (Por eso yo las
-          detono en mi cuarto Y en ese culito va el sello) Las morras me besan,
-          pues las vuelvo locas a todas Se besan, ellas bailan y se alborotan No
-          ocupan flores, solo un polvo que es rosa Y sigo bendecido, con lo mío
-          en el camino, oh
-        </p>
-      </div>
-      <div class="others-reference">
-        <h2 class="others">otros artistas</h2>
-        <ul class="others-container">
-          <li class="disc-others">
-            <img :src="songs.datanext1.cover_medium" alt="" class="disc-next" />
-          </li>
-          <li class="disc-others">
-            <img :src="songs.datanext2.cover_medium" alt="" class="disc-next" />
-          </li>
-          <li class="disc-others">
-            <img :src="songs.datanext3.cover_medium" alt="" class="disc-next" />
-          </li>
-          <li class="disc-others">
-            <img :src="songs.datanext4.cover_medium" alt="" class="disc-next" />
-          </li>
-        </ul>
-      </div>
-    </div>
   </div>
 </template>
 <script>
 import { useMusicStore } from "../stores/musicStore";
-
 export default {
   name: "BaseFront",
   component: {},
@@ -100,8 +83,8 @@ export default {
 
   methods: {
     playSound(preview) {
-      let audio = new Audio(preview);     audio.play();
-      console.log(audio);
+      let audio = new Audio(preview);
+      audio.play()
     },
   },
 };
--- src/components/stores/musicStore.js
+++ src/components/stores/musicStore.js
@@ -1,105 +1,57 @@
 import { defineStore } from "pinia";
+
 export const useMusicStore = defineStore({
   id: "musicStore",
   state() {
     return {
       data: [],
-      artist: [],
-      datanext1: [],
-      datanext2: [],
-      datanext3: [],
-      datanext4: [],
-      songs: [],
+      tracks: [],
       value: null,
+      image: undefined,
     };
   },
 
   actions: {
     async setSongs() {
       this.getRandomInt();
-      this.nextArtists1();
-      this.nextArtists2();
-      this.nextArtists3();
-      this.nextArtists4();
-
-      await fetch(`https://api.deezer.com/album/${this.value}`)
-        .then((response) => response.json())
-        .then((data) => {
-          this.data = data;
-          this.artist = data.artist;
-          this.songs = data.tracks.data;
-          console.log(data);
-          console.log(this.songs);
-        })
-        .catch(() => {
-          this.setSongs();
-        });
-    },
-
-    async nextArtists1() {
-      let min = Math.ceil(93000);
-      let max = Math.floor(974178);
-      this.x = Math.floor(Math.random() * (max - min) + min);
-      await fetch(`https://api.deezer.com/album/${this.x}`)
-        .then((response) => response.json())
-        .then((data) => {
-          this.datanext1 = data;
-        })
-        .catch((error) => {
-          console.log(error);
-          this.nextArtists1();
-        });
-    },
-    async nextArtists2() {
-      let min = Math.ceil(93000);
-      let max = Math.floor(974178);
-      this.x = Math.floor(Math.random() * (max - min) + min);
-      await fetch(`https://api.deezer.com/album/${this.x}`)
-        .then((response) => response.json())
-        .then((data) => {
-          this.datanext2 = data;
-        })
-        .catch((error) => {
-          console.log(error);
-          this.nextArtists2();
-        });
-    },
-    async nextArtists3() {
-      let min = Math.ceil(93000);
-      let max = Math.floor(974178);
-      this.x = Math.floor(Math.random() * (max - min) + min);
-      await fetch(`https://api.deezer.com/album/${this.x}`)
-        .then((response) => response.json())
-        .then((data) => {
-          this.datanext3 = data;
-        })
-        .catch((error) => {
-          console.log(error);
-          this.nextArtists3();
-        });
-    },
-    async nextArtists4() {
-      let min = Math.ceil(93000);
-      let max = Math.floor(974178);
-      this.x = Math.floor(Math.random() * (max - min) + min);
-      await fetch(`https://api.deezer.com/album/${this.x}`)
-        .then((response) => response.json())
-        .then((data) => {
-          this.datanext4 = data;
-        })
-        .catch((error) => {
-          console.log(error);
-          this.nextArtists4();
-        });
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "eeb82b93e9msh1cd098fca9b6e27p1fae80jsn9df333442437",
+          "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+        },
+      };
+      try {
+        await fetch(
+          `https://deezerdevs-deezer.p.rapidapi.com/album/${this.value}`,
+          options
+        )
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            this.data = data;
+            this.tracks = data.tracks.data;
+            this.image = data.cover_medium;
+          });
+      } catch (error) {
+        this.setSongs();
+        console.error(error);
+      }
     },
 
     getRandomInt(min, max) {
-      min = Math.ceil(93000);
-      max = Math.floor(974178);
+      min = Math.ceil(95000);
+      max = Math.floor(950000);
       this.value = Math.floor(Math.random() * (max - min) + min);
       return this.value;
     },
 
-  
+    playSound(preview) {
+      this.audio = new Audio(preview);
+      this.audio.play();
+      console.log(this.audio);
+    },
   },
 });
